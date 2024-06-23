21fa066969674190795ad427794946432bf1ac69
Krishan0202 <krishankataria0202@gmail.com>
2024-06-24 00:02:07 +0530

    add page 3

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
+
+let elemContainer = document.querySelector("#elem-container");
+let fixedImage = document.querySelector("#fixed-image");
+elemContainer.addEventListener("mouseenter", function () {
+  fixedImage.style.display = "block";
+});
+elemContainer.addEventListener("mouseleave", function () {
+  fixedImage.style.display = "none";
+});
+
+// let elem1 = document.querySelector("#elem1");
+
+// elem1.addEventListener("mouseenter", function () {
+//   let image = elem1.getAttribute("data-image");
+//   fixedImage.style.backgroundImage = `url(${image})`;
+// });
+
+let elems = document.querySelectorAll(".elem");
+
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    let image = e.getAttribute("data-image");
+    fixedImage.style.backgroundImage = `url(${image})`;
+  });
 });
