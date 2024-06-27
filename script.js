5f9780bee716b14223540693729db9464457927d
Krishan0202 <krishankataria0202@gmail.com>
2024-06-27 14:42:21 +0530

    responsive pending

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector("#main"),
-//   smooth: true,
-// });
-
-let elemContainer = document.querySelector("#elem-container");
-let fixedImage = document.querySelector("#fixed-image");
-elemContainer.addEventListener("mouseenter", function () {
-  fixedImage.style.display = "block";
-});
-elemContainer.addEventListener("mouseleave", function () {
-  fixedImage.style.display = "none";
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-// let elem1 = document.querySelector("#elem1");
-
-// elem1.addEventListener("mouseenter", function () {
-//   let image = elem1.getAttribute("data-image");
-//   fixedImage.style.backgroundImage = `url(${image})`;
-// });
-
-let elems = document.querySelectorAll(".elem");
-
-elems.forEach(function (e) {
-  e.addEventListener("mouseenter", function () {
-    let image = e.getAttribute("data-image");
-    fixedImage.style.backgroundImage = `url(${image})`;
+function page4Animation() {
+  let elemContainer = document.querySelector("#elem-container");
+  let fixedImage = document.querySelector("#fixed-image");
+  elemContainer.addEventListener("mouseenter", function () {
+    fixedImage.style.display = "block";
   });
-});
+  elemContainer.addEventListener("mouseleave", function () {
+    fixedImage.style.display = "none";
+  });
+
+  let elems = document.querySelectorAll(".elem");
+
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      let image = e.getAttribute("data-image");
+      fixedImage.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+  });
+}
+swiperAnimation();
+page4Animation();
